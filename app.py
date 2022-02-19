01d34b2c9faaf0979f1e4286fff03bb3bd86a634
TeYuanLiu <teyuanliu@gmail.com>
2022-02-19 22:44:10 +0800

    add easy/hard mode feature

--- app.py
+++ app.py
@@ -12,15 +12,14 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 def factory():    
-    args = dotdict({
-        'numEpochs': 4,                 # number of epochs
-        'numIterations': 20,            # number of self-played games per epoch
-        'numMCTSRuns': 40,              # number of MCTS runs per move in game
-        'numBattles': 20,               # number of games for bot replacement
-        'replaceNetThreshold': 0.55,    # win rate threshold for bot replacement        
-        'cpuct': 1,                     # upper confidence bound exploration hyperparameter 
-        'maxNumEpochTrainExamples': 4,  # number of epochs recorded
-        'tempThreshold': 4,             # number of exploration moves before picking the move with the maximum simulation counts in game
+    argsEasy = dotdict({
+        'numMCTSRuns': 5,              # number of MCTS runs per move in game    
+        'cpuct': 1,                     # upper confidence bound exploration hyperparameter
+    })
+
+    argsHard = dotdict({
+        'numMCTSRuns': 20,              # number of MCTS runs per move in game    
+        'cpuct': 1,                     # upper confidence bound exploration hyperparameter
     })
 
     netArgs = dotdict({
@@ -30,11 +29,16 @@ def factory():
     })
 
     game = TicTacToe()
-    net = TicTacToeNet(game, netArgs)   
-    bot = Bot(game, net, args)
-    botDirectoryPath = "./experiments/2021-12-31-15:54:17/bot3"
-    bot.load(botDirectoryPath)
-    
+
+    netEasy = TicTacToeNet(game, netArgs)   
+    botEasy = Bot(game, netEasy, argsEasy)
+    botEasyDirectoryPath = "./experiments/2022-02-19-21:59:00/bot0"
+    botEasy.load(botEasyDirectoryPath)
+
+    netHard = TicTacToeNet(game, netArgs)   
+    botHard = Bot(game, netHard, argsHard)
+    botHardDirectoryPath = "./experiments/2021-12-31-15:54:17/bot3"
+    botHard.load(botHardDirectoryPath)
 
     app = Flask(__name__)
     CORS(app)
@@ -43,20 +47,37 @@ def factory():
     def welcome():
         return f'Welcome to the Alpha TicTacToe Zero API'
 
-    @app.route("/api/<query>")
-    def handle_query(query=None):
+    @app.route("/api/easy/<query>")
+    def handle_query_easy(query=None):
         board = np.zeros((3, 3))
         if not (query and len(query) == 9): return f'query length is not valid'
         for r in range(3):
             for c in range(3):
                 d = query[3*r + c]
-                if d == 'O': board[r][c] = 1
-                elif d == 'X': board[r][c] = -1
+                if d == 'o': board[r][c] = 1
+                elif d == 'x': board[r][c] = -1
                 elif d == '-': continue
                 else: return f'query symbol at index {r * 3 + c} is not valid'
         if game.get_outcome(board, 1) != None: return f'game has ended'
         canonicalBoard = game.get_canonical_board(board, 1)
-        pi = bot.get_pi(canonicalBoard, temp=0)
+        pi = botEasy.get_pi(canonicalBoard, temp=0)
+        a = np.random.choice(len(pi), p=pi)
+        return f'{a}'
+
+    @app.route("/api/hard/<query>")
+    def handle_query_hard(query=None):
+        board = np.zeros((3, 3))
+        if not (query and len(query) == 9): return f'query length is not valid'
+        for r in range(3):
+            for c in range(3):
+                d = query[3*r + c]
+                if d == 'o': board[r][c] = 1
+                elif d == 'x': board[r][c] = -1
+                elif d == '-': continue
+                else: return f'query symbol at index {r * 3 + c} is not valid'
+        if game.get_outcome(board, 1) != None: return f'game has ended'
+        canonicalBoard = game.get_canonical_board(board, 1)
+        pi = botHard.get_pi(canonicalBoard, temp=0)
         a = np.random.choice(len(pi), p=pi)
         return f'{a}'
 
